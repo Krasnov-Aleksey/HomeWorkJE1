759aec9a756ff9830c234d61efc5556281c003bf
aleksey.krasnov <krasnov.80@mail.ru>
2023-08-31 14:25:21 +0300

    task2

---
+++ src/task2/Task2.java
@@ -0,0 +1,20 @@
+package task2;
+
+import static java.lang.Double.POSITIVE_INFINITY;
+
+public class Task2 {
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            double [] intArray = {1,2,3,4,5,6,7,8,9};
+            double catchedRes1 = intArray[8] / d;
+            if(catchedRes1 == POSITIVE_INFINITY)
+                throw new ArithmeticException();
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Выход за пределы массива");
+        }
+    }
+}
